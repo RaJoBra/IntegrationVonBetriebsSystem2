6f850ba3145a3c0ceedae7cc3e22b1327adb964f
unknown <bebe1020@hs-karlsruhe.de>
2021-12-29 20:02:58 +0100

    Change Product Name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ProduktionsplanungsTool'`, () => {
+  it(`should have as title 'Production Wizzard'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ProduktionsplanungsTool');
+    expect(app.title).toEqual('Production Wizzard');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ProduktionsplanungsTool app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Production Wizzard app is running!');
   });
 });
